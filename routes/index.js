2798e9682ef8eeeaa72044a87a8992e355f0ddc7
Atul Malaviya <atulmal@microsoft.com>
2018-04-27 20:09:39 +0530

    Cleaned up the app and added back liveliness probe

--- routes/index.js
+++ routes/index.js
@@ -2,32 +2,12 @@
 var express = require('express');
 var router = express.Router();
 var redis = require('ioredis');
+
 console.log("trying to create redis client");
-// var redisServer = process.env.redis_server || 'redis-cache';
-var client = redis.createClient(6379, 'redis-cache');
+var redisServer = process.env.redis_server || 'redis-cache';
+console.log(redisServer);
+var client = redis.createClient(6379, redisServer);
 console.log("redis client created");
-
-
-// var redis = new Redis(6379, 'redis-cache');
-
-// var redis = new Redis({
-//     port: 6379,          // Redis port
-//     host: 'atulm-redis.redis.cache.windows.net',   // Redis host
-//     family: 4,           // 4 (IPv4) or 6 (IPv6)
-//     password: 'Y1cPTLrbcwlSkAT4c56m/0j6dv2CrAMU/LmQ6+zcelE=',
-//     db: 0
-//   })
-
-console.log('Starting ioredis test');
-client.set('foo', 'bar');
-console.log('ioredis set completed');
-client.get('foo', function (err, result) {
-  console.log('ioredis get completed');  
-  console.log(result);
-});
-
-
-var fromCache = "Hello world";
 
 /* GET home page. */
 router.get('/', function (req, res) {
@@ -35,8 +15,8 @@ router.get('/', function (req, res) {
     client.get('foo', function (err, result) {
       console.log('ioredis get completed');  
       console.log(result);
+      res.render('index', { title: result });
     });     
-    res.render('index', { title: fromCache[0] });
 });
 
 module.exports = router;
